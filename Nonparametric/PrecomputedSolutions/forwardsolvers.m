fd2d6366e28f4570838a3a61faae987081e4c7eb
erutter <36344291+erutter@users.noreply.github.com>
2020-02-05 14:20:51 -0800

    Add files via upload

--- Nonparametric/PrecomputedSolutions/forwardsolvers.m
+++ Nonparametric/PrecomputedSolutions/forwardsolvers.m
@@ -6,21 +6,19 @@ function forwardsolvers(param,precomputed_sol,X)
 % 
 %    INPUTS:
 %        param: a variable that contains the following information:
-%            -param.x_pde: the x vector for solutions
-%            -param.t_pde: the t vector for solutions
-%            -param.m: m required for pdepe (based on symmetry)
-%            -param.bsD: vector of nodes for parameter 1 on which to solve
+%            -param.xs: the x vector for solutions
+%            -param.ys: the y vector for solutions
+%            -param.t:  the t vector for solutions
+%            -param.bsr: vector of nodes for parameter 'r' on which to solve
 %                the pde
-%            -param.bsr: vector of nodes for parameter 2 on which to solve
-%                the pde
-%  
+%        X: a matrix of times, xs, and ys for the solution
 %        precomputed_sol: the savename of the solutions
 %
 %    OUTPUTS:
 %        precomputed_sol: a mat-file with the full forward solutions over  
-%             the parameter space of interest
+%             the parameter space of interest (named 'fullsol')
 %
-% Written by Erica Rutter (July 2017)
+% Written by Erica Rutter (July 2019)
 
 fullsol=zeros(length(param.xs),length(param.ys),length(param.t),length(param.bsr));
 for k =1:length(param.bsr)
